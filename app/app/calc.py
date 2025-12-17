9db79cf4a49800cc156e8c5a07ee1d2a26477ca2
Md Juel Rana <md.juelrana0412@gmail.com>
2025-12-17 20:41:20 +0600

    configered wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,8 @@
+"""
+Calculator function
+"""
+def add(x, y):
+    return x + y
+
+def subtrac(x, y):
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""
+Simple test
+"""
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtrac_numbers(self):
+        res = calc.subtrac(15, 10)
+
+        self.assertEqual(res, 5)
